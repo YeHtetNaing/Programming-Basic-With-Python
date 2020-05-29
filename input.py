c7eac5e804d98d78db6611ea54266845f203d2ad
YeHtet <65211258+YeHtetNaing@users.noreply.github.com>
2020-05-28 20:26:49 -1200

    Add file from chapter 1

---
+++ input.py
@@ -0,0 +1,4 @@
+input1 = int(input("Please enter first number: "))
+input2 = int(input("Pleade enter second number: "))
+result = input1 + input2 # int()---> string from integer
+print (input1,"+",input2,"=",result)
