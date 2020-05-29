9026c52052d11d6f4e0f247fe5235cd058eb5845
YeHtet <65211258+YeHtetNaing@users.noreply.github.com>
2020-05-28 19:49:57 -1200

    Add file from chapter 1

---
+++ Operators.py
@@ -0,0 +1,32 @@
+# (+)
+k = 5 + 4
+print (k)
+
+a = 3
+b = 4
+c = a + b
+print (c)
+
+# (-)
+a = 10
+b = 4
+c = a - b
+print (c)
+
+# (*)
+a = 3 
+b = 4
+c = a * b
+print (c)
+
+# (/)
+a = 10
+b = 2
+c = a / b
+print (c)
+
+# (%)
+a = 13
+b = 8
+c = a % b
+print (c)
